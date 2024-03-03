fe94f8c878da2d700f03343eeb42d5b1e1932e8c
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-03-03 11:10:16 +0100

    new commit

--- app/src/main/java/com/example/jezyki_programowania_projekt/MainActivity.kt
+++ app/src/main/java/com/example/jezyki_programowania_projekt/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.jezyki_programowania_projekt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jezyki_programowania_projekt.ui.theme.Jezyki_programowania_projektTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
             Jezyki_programowania_projektTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    GreetingWithCounter()
                 }
             }
         }
@@ -27,10 +36,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingWithCounter() {
+    var counter by remember { mutableIntStateOf(0) }
+    Column {
+        Greeting(name = "Piotr")
+        Text(
+            text = "Counter: $counter",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = { counter++ }) {
+            Text("Increment Counter")
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = "Hello $name!",
+        modifier = Modifier.padding(16.dp)
     )
 }
 
@@ -38,6 +63,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Jezyki_programowania_projektTheme {
-        Greeting("Android")
+        GreetingWithCounter()
     }
 }
